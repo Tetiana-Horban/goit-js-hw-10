e63ea69b47f5512cc4bdfd02ae02b90b67a03538
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-11-09 20:32:26 +0200

    Delete fetchCountries.js

--- js/services/fetchCountries.js
+++
@@ -1,8 +0,0 @@
-export function fetchCountries(country) {
-  return fetch('https://restcountries.com/v3.1/name/${country}').then(r => {
-    if (!r.ok) {
-      throw Error(r.statusText);
-    }
-    return r.json();
-  });
-}
